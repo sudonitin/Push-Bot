2d5b8db35b0a826b8a8ae27908de2243c7338e21
globefire <sahun5774@gmail.com>
2019-08-12 20:06:17 +0530

    debugging

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ f.write(strgen.StringGenerator("[\d\w]{3}").render())
 f.close()
 
 print("machayenge")
+import os
+os.system("git add .")
+os.system('git commit -m "some chang"')
+os.system('git push origin master')
