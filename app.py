bb72c1010b8ec5418e002fb1f674da784139500a
globefire <sahun5774@gmail.com>
2019-08-14 19:38:27 +0530

    changes in app

--- app.py
+++ app.py
@@ -5,9 +5,3 @@ f.write(strgen.StringGenerator("[\d\w]{3}").render())
 f.close()
 
 print("machayenge")
-import os
-os.system('git init')
-os.system('git remote add origin https://github.com/globefire/Goals.git')
-os.system('git add .')
-os.system('git commit -m "some chang"')
-os.system('git push origin master')
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 python ./app.py
 git init
+git remote add origin https://github.com/globefire/Goals.git
 git add .
 git commit -m "a new commit"
 git push origin master
