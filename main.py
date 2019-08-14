9b7c2506e2b457adf0e533dcfc8c0ae3ec92605f
globefire <sahun5774@gmail.com>
2019-08-14 17:35:02 +0530

    some chang

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=24)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=17, minute=35)
 def scheduled_job():
     os.system("run.sh")
 
