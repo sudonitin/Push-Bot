9fbeba47dc868a8e04db424acdbb4d460fc23af9
globefire <sahun5774@gmail.com>
2019-08-14 17:51:15 +0530

    pushing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
+#the line below executes the scheduled_job function everyday at 5:35pm
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=17, minute=35)
 def scheduled_job():
     os.system("run.sh")
